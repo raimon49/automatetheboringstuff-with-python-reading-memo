1771aad0d2c9ef1fe18ac5f5cf2e4cab390e298e
raimon <raimon49@hotmail.com>
2023-03-09 21:02:22 +0900

    Add an annotate comment about Ch 1.5

--- ch01/1.5_first_program.py
+++ ch01/1.5_first_program.py
@@ -4,14 +4,14 @@
 
 def main():
     print('Hello world!')
-    print('What is your name?')
+    print('What is your name?') # 名前を尋ねる
     my_name = input()
     print('It is good to meet you, ' + my_name)
-    print('The length of your name is:')
+    print('The length of your name is:') # 名前の長さを表示
     print(len(my_name))
-    print('Wthat is your age?')
+    print('Wthat is your age?') # 年齢を尋ねる
     my_age = input()
-    print('You will be ' + str(int(my_age) + 1) + ' in a year.')
+    print('You will be ' + str(int(my_age) + 1) + ' in a year.') # 来年の年齢を表示
 
 if __name__ == '__main__':
     main()
