bf5e0b897e21469486f4997fa34b2e95d9d6d41a
raimon <raimon49@hotmail.com>
2023-03-18 19:02:58 +0900

    Add a code about Ch 3.8

---
+++ ch03/3.8_guessTheNumber.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+import random
+
+def main():
+    secret_number = random.randint(1, 20)
+    print('1から20までの数を当ててください。')
+
+    for guessess_taken in range(1, 7):
+        print('数を入力してください。')
+        guess = int(input())
+
+        if guess < secret_number:
+            print('小さいです。')
+        elif guess > secret_number:
+            print('大きいです。')
+        else:
+            break
+
+    if guess == secret_number:
+        print('大当たり！' + str(guessess_taken) + '回で当たりました！')
+    else:
+        print('残念。正解は' + str(secret_number) + 'でした。')
+
+if __name__ == '__main__':
+    main()
+
