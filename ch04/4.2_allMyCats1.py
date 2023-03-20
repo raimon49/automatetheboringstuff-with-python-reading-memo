d9521b64a2b630a3f72fafb8bbef1e52586d3a81
raimon <raimon49@hotmail.com>
2023-03-21 00:41:48 +0900

    Add an annotate comment about Ch 4.2

--- ch04/4.2_allMyCats1.py
+++ ch04/4.2_allMyCats1.py
@@ -11,7 +11,7 @@ def main():
         name = input()
         if name == '':
             break
-        cat_names = cat_names + [name]
+        cat_names = cat_names + [name] # リストの連結
     print('猫の名前は次のとおり:')
     for name in cat_names:
         print('  ' + name)
