24db8507767ae9f494a4321f3d5b94a1a7d4c343
raimon <raimon49@hotmail.com>
2023-03-24 00:04:38 +0900

    Add an annotate comment about Ch 4.2

--- ch04/4.2_allMyCats1.py
+++ ch04/4.2_allMyCats1.py
@@ -9,7 +9,7 @@ def main():
         print('猫' + str(len(cat_names) + 1) + 'の名前を入力してください' +
               '（終了するにはEnterキーだけ押してください）')
         name = input()
-        if name == '':
+        if name == '': # Enterキーだけ押されて何も入力されていない
             break
         cat_names = cat_names + [name] # リストの連結
     print('猫の名前は次のとおり:')
