96aec339a9ba7af13ffc098c88f3919979b8aa80
raimon <raimon49@hotmail.com>
2023-03-25 16:20:57 +0900

    Add a code about Ch 4.2.2

---
+++ ch04/4.2.2_myPets.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+import random
+
+def main():
+    my_pets = ['Zophie', 'Pooka', 'Fat-tail']
+    print('ペットの名前を入力してください')
+    name = input()
+    if name not in my_pets:
+        print(name + 'という名前のペットは飼っていません')
+    else:
+        print(name + 'は私のペットです')
+
+if __name__ == '__main__':
+    main()
+
