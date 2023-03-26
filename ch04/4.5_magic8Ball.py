fe739a8f46209c656b0ec87792224ac957d1358f
raimon <raimon49@hotmail.com>
2023-03-26 19:27:06 +0900

    Add a code about Ch 4.5

---
+++ ch04/4.5_magic8Ball.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+import random
+
+def main():
+    messages = [
+        '確かにそうだ',
+        '間違いなくそうだ',
+        'はい',
+        'なんとも。もういちどやってみて',
+        'あとでもう一度聞いてみて',
+        '集中してもう一度聞いてみて',
+        '私の答えはノーです',
+        '見通しはそれほどよくない',
+        'とても疑わしい',
+    ]
+    print(messages[random.randint(0, len(messages) - 1)])
+
+if __name__ == '__main__':
+    main()
+
