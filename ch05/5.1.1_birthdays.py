32f4e1d63d6a46a6da860eee80082ca160823e6c
raimon <raimon49@hotmail.com>
2023-03-29 20:42:13 +0900

    Add a code about Ch 5.1.1

---
+++ ch05/5.1.1_birthdays.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+import random
+
+def main():
+    birthdays = {
+        'アリス': '4/1',
+        'ボブ': '12/12',
+        'キャロル': '4/4',
+    }
+
+    while True:
+        print('名前を入力してください（終了するにはEnterだけ押してください）')
+        name = input()
+        if name == '':
+            break
+
+        if name in birthdays:
+            print(name + 'の誕生日は' + birthdays[name])
+        else:
+            print(name + 'の誕生日は未登録です。')
+            print('誕生日を入力してください')
+            bday = input()
+            birthdays[name] = bday
+            print('誕生日データベースを更新しました。')
+
+if __name__ == '__main__':
+    main()
+
