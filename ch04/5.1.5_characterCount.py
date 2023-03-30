55b9328aa3118f9b9c5dacdda26477bcb87ece7e
raimon <raimon49@hotmail.com>
2023-03-30 20:23:33 +0900

    Add a code about Ch 5.1.5

---
+++ ch04/5.1.5_characterCount.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+import random
+
+def main():
+    message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+    count = {}
+
+    for character in message:
+        count.setdefault(character, 0)
+        count[character] = count[character] + 1
+
+    print(count)
+
+if __name__ == '__main__':
+    main()
+
