a33ae3d6a3e5520368bbc15faa60962aa9b78fce
raimon <raimon49@hotmail.com>
2023-03-31 18:33:38 +0900

    Remove unused import random

--- ch04/5.1.5_characterCount.py
+++ ch04/5.1.5_characterCount.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
-import random
 
 def main():
     message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
