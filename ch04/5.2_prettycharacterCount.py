9da49a234a1f17e05a91aa1b0915e82539e4a266
raimon <raimon49@hotmail.com>
2023-03-31 18:34:07 +0900

    Add a code about Ch 5.2

---
+++ ch04/5.2_prettycharacterCount.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+import pprint
+
+def main():
+    message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+    count = {}
+
+    for character in message:
+        # if character in count:
+        #     count[character] = 0
+        # 上記のコードと同義
+        count.setdefault(character, 0)
+        count[character] = count[character] + 1
+
+    pprint.pprint(count)
+
+if __name__ == '__main__':
+    main()
+
