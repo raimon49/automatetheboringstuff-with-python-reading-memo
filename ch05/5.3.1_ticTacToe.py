972dd8ab780cd557b1076c810430114b1a70335d
raimon <raimon49@hotmail.com>
2023-04-06 20:02:09 +0900

    Update a code about Ch 5.3.1
    Set mark in the board

--- ch05/5.3.1_ticTacToe.py
+++ ch05/5.3.1_ticTacToe.py
@@ -3,9 +3,9 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
-    the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-                 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-                 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+    the_board = {'top-L': 'O', 'top-M': 'O', 'top-R': 'O',
+                 'mid-L': 'X', 'mid-M': 'X', 'mid-R': ' ',
+                 'low-L': ' ', 'low-M': ' ', 'low-R': 'X'}
 
     def print_board(board):
         print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
