664bfbb2c5d839f443cd2d0db10a0cea7c33866c
raimon <raimon49@hotmail.com>
2023-04-09 09:48:03 +0900

    Update a code about Ch 5.3.1

--- ch05/5.3.1_ticTacToe.py
+++ ch05/5.3.1_ticTacToe.py
@@ -3,9 +3,9 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
-    the_board = {'top-L': 'O', 'top-M': 'O', 'top-R': 'O',
-                 'mid-L': 'X', 'mid-M': 'X', 'mid-R': ' ',
-                 'low-L': ' ', 'low-M': ' ', 'low-R': 'X'}
+    the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+                 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+                 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
     def print_board(board):
         print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
@@ -14,7 +14,16 @@ def main():
         print('-+-+-')
         print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
-    print_board(the_board)
+    turn = 'X'
+    for i in range(9):
+        print_board(the_board)
+        print(turn + 'の番です。どこに打つ？')
+        move = input()
+        the_board[move] = turn
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
 
 
 if __name__ == '__main__':
