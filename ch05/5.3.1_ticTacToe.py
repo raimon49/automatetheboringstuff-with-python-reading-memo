9979a2349c801d29d93f58d86bea1d7859f24463
raimon <raimon49@hotmail.com>
2023-04-11 19:17:28 +0900

    Add an annotate comment about Ch 5.3.1

--- ch05/5.3.1_ticTacToe.py
+++ ch05/5.3.1_ticTacToe.py
@@ -3,11 +3,13 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
+    # 三目並べのデータ構造
     the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
                  'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
                  'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
     def print_board(board):
+        # ゲーム状況の出力
         print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
         print('-+-+-')
         print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
