b468ad8b9820409f3426032cf1d208f65a0ae6b6
raimon <raimon49@hotmail.com>
2023-04-12 19:08:57 +0900

    Add a code about Ch 5.3.2

---
+++ ch05/5.3.2_nestedDict.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    all_guests = {
+        'アリス': {'リンゴ': 5, 'プレッツェル': 12},
+        'ボブ': {'ハムサンド': 3, 'リンゴ': 2},
+        'キャロル': {'コップ': 3, 'アップルパイ': 1},
+    }
+
+    def total_brought(guests, item):
+        num_brought = 0
+        for k, v in guests.items():
+            num_brought = num_brought + v.get(item, 0)
+
+        return num_brought
+
+    print('持ち物の数')
+    print('  -  リンゴ ' + str(total_brought(all_guests, 'リンゴ')))
+
+
+if __name__ == '__main__':
+    main()
+
