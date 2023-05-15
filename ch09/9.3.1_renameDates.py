a531850e49ec5aba36b38d8ce1489734a359995b
raimon <raimon49@hotmail.com>
2023-05-15 19:12:35 +0900

    Add a code about Ch. 9.3.1

---
+++ ch09/9.3.1_renameDates.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import shutil, os, re
+
+    # TODO: 米国式日付のファイル名にマッチする正規表現を作る
+    # TODO: カレントディレクトリの全ファイルをループする
+    # TODO: 日付のないファイルをスキップする
+    # TODO: ファイル名を部分分解する
+    # TODO: 欧州式の日付ファイルを作る
+    # TODO: ファイル名を変更する
+
+
+if __name__ == '__main__':
+    main()
+
