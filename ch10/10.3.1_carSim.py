fdc4139dd0ec02ede89e215d70ef3dda9344ed77
raimon <raimon49@hotmail.com>
2023-06-02 18:30:07 +0900

    Add a code about Ch. 10.3.1

---
+++ ch10/10.3.1_carSim.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    market_2nd = {'ns': 'green', 'ew': 'red'}
+    mission_16th = {'ns': 'red', 'ew': 'green'}
+
+    def switch_lights(stoplight):
+        for key in stoplight.keys():
+            if stoplight[key] == 'green':
+                stoplight[key] = 'yellow'
+            elif stoplight[key] == 'yellow':
+                stoplight[key] = 'red'
+            elif stoplight[key] == 'red':
+                stoplight[key] = 'green'
+
+    switch_lights(market_2nd)
+
+if __name__ == '__main__':
+    main()
+
