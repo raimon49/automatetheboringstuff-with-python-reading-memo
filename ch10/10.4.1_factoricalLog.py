6676b91287583520f520061abbb966c6f262f2f1
raimon <raimon49@hotmail.com>
2023-06-04 18:22:46 +0900

    Fix bug about Ch. 10.3.1

--- ch10/10.4.1_factoricalLog.py
+++ ch10/10.4.1_factoricalLog.py
@@ -11,7 +11,7 @@ def main():
     def factorial(n):
         logging.debug('factorial({})開始'.format(n))
         total = 1
-        for i in range(n + 1):
+        for i in range(1, n + 1):
             total *= i
             logging.debug('i = {}, total = {}'.format(i, total))
 
