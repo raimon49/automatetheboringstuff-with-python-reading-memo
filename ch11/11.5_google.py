823279dc7abb2601e3394475e036a22d66cef231
raimon <raimon49@hotmail.com>
2023-06-14 17:49:20 +0900

    Add a code about Ch. 11.5

--- ch11/11.5_google.py
+++ ch11/11.5_google.py
@@ -3,36 +3,12 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
-    import  bs4
+    import requests, sys, bs4
 
-    example_file = open('example.html')
-    example_soup = bs4.BeautifulSoup(example_file)
-
-    # 属性の探索
-    elems = example_soup.select('#author')
-    print(type(elems))
-    print(len(elems))
-    print(type(elems[0]))
-    print(elems[0].getText())
-    print(str(elems[0]))
-    print(elems[0].attrs)
-
-    # タグの探索
-    p_elems = example_soup.select('p')
-    print(len(p_elems))
-    print(str(p_elems[0]))
-    print(p_elems[0].getText())
-    print(str(p_elems[1]))
-    print(p_elems[1].getText())
-    print(str(p_elems[2]))
-    print(p_elems[2].getText())
-
-    # Tagオブジェクトのget()メソッドを用いた探索
-    span_elem = example_soup.select('span')[0]
-    print(str(span_elem))
-    print(span_elem.get('id'))
-    print(span_elem.get('some_nonexistent_addr') == None)
-    print(span_elem.attrs)
+    print('Googling...')
+    # res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
+    res = requests.get('https://google.com/search?q=toyota')
+    res.raise_for_status()
 
 if __name__ == '__main__':
     main()
