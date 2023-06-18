7a115b65b1e170da7b534c05aeb4758ffa33b33a
raimon <raimon49@hotmail.com>
2023-06-18 18:44:57 +0900

    Add a code about Ch. 11.6

--- ch11/11.6_downloadXkcd.py
+++ ch11/11.6_downloadXkcd.py
@@ -8,6 +8,15 @@ def main():
     res = requests.get('https://xkcd.com')
     res.raise_for_status()
 
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    comic_elem = soup.select('#comic img')
+    if comic_elem == []:
+        print('コミック画像が見つかりませんでした')
+    else:
+        print('コミック画像が見つかりました')
+        comic_url = 'https:' + comic_elem[0].get('src')
+        print(comic_url)
+
 
 if __name__ == '__main__':
     main()
