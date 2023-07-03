a9f0739d6496c44073dc6028a589fd2e94cdac90
raimon <raimon49@hotmail.com>
2023-07-04 00:14:53 +0900

    Add a code about Ch. 12.4

--- ch12/12.4_readCensusExcel.py
+++ ch12/12.4_readCensusExcel.py
@@ -8,6 +8,14 @@ def main():
     print('ワークブックを開いています...')
     wb = openpyxl.load_workbook('censuspopdata.xlsx')
     print(wb.sheetnames)
+    sheet = wb['Population by Census Tract']
+    country_cata = {}
+
+    print('行を読み込んでいます...')
+    for row in range(2, sheet.max_row + 1):
+        state = sheet['B' + str(row)].value
+        country = sheet['C' + str(row)].value
+        pop = sheet['D' + str(row)].value
 
 if __name__ == '__main__':
     main()
