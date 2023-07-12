43b47339a92801ad8c2668e7b4bf8921b4658dbd
raimon <raimon49@hotmail.com>
2023-07-12 18:57:30 +0900

    Add a code about Ch 12.6

--- ch12/12.6_updateProduceSalesFile.py
+++ ch12/12.6_updateProduceSalesFile.py
@@ -8,6 +8,11 @@ def main():
     wb = openpyxl.load_workbook('produceSales.xlsx')
     sheet = wb['Sheet']
 
+    # 農産物の種類と、更新する価格
+    PRICE_UPDATES = {'Garlic': 3.07,
+                     'Celery': 1.19,
+                     'Lemon': 1.27}
+
 if __name__ == '__main__':
     main()
 
