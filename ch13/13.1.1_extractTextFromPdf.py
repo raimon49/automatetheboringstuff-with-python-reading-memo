273432af1586e113464050f120d47ade08cb9d1b
raimon <raimon49@hotmail.com>
2023-07-25 18:11:56 +0900

    Add a code about Ch 13.1.1

---
+++ ch13/13.1.1_extractTextFromPdf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import PyPDF2
+
+    pdf_file_obj = open('meetingminutes.pdf', 'rb')
+    pdf_reader = PyPDF2.PdfReader(pdf_file_obj)
+
+if __name__ == '__main__':
+    main()
+
