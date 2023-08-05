3370d050b2eae19596aa87eee238370aab812346
raimon <raimon49@hotmail.com>
2023-08-05 19:08:53 +0900

    Add a code about Ch 13.1.3.3

---
+++ ch13/13.1.3.3_watermarkPdf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import PyPDF2
+
+    minutes_file = open('meetingminutes.pdf', 'rb')
+    pdf_reader = PyPDF2.PdfReader(minutes_file)
+    minutes_first_page = pdf_reader.pages[0]
+    pdf_watermark_reader = PyPDF2.PdfReader(open('watermark.pdf', 'rb'))
+    minutes_first_page.merge_page(pdf_watermark_reader.pages[0])
+
+if __name__ == '__main__':
+    main()
+
