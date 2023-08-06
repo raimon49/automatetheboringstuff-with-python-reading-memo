357c4ad6999581b45294c5f3e0d47f0236dba125
raimon <raimon49@hotmail.com>
2023-08-06 23:02:12 +0900

    Add a code about Ch 13.1.3.3

--- ch13/13.1.3.3_watermarkPdf.py
+++ ch13/13.1.3.3_watermarkPdf.py
@@ -12,6 +12,12 @@ def main():
     # 別PDFのページを重ねて透かしを埋め込む
     minutes_first_page.merge_page(pdf_watermark_reader.pages[0])
 
+    pdf_writer = PyPDF2.PdfWriter() # 白紙のPDF文書を作成
+    pdf_writer.add_page(minutes_first_page)
+    for page_num in range(1, len(pdf_reader.pages)):
+        page_obj = pdf_reader.pages[page_num]
+        pdf_writer.add_page(page_obj)
+
 if __name__ == '__main__':
     main()
 
