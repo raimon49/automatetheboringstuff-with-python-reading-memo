193d2955d91971bdbdac65ce4fb9fc64ae22cf27
raimon <raimon49@hotmail.com>
2023-08-08 22:39:37 +0900

    Add a code about Ch 13.1.3.4

---
+++ ch13/13.1.3.4_encryptPdf.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import PyPDF2
+
+    # PdfReaderオブジェクトを生成
+    pdf_file_obj = open('meetingminutes.pdf', 'rb')
+    pdf_reader = PyPDF2.PdfReader(pdf_file_obj)
+    pdf_writer = PyPDF2.PdfWriter()
+
+    pdf_writer.encrypt('sowrdfish')
+
+if __name__ == '__main__':
+    main()
+
