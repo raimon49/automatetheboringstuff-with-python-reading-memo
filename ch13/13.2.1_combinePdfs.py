08dc98b15545e9619aeebe06e0418267b2ce57da
raimon <raimon49@hotmail.com>
2023-08-11 18:45:54 +0900

    Add a code about Ch 13.2.1

---
+++ ch13/13.2.1_combinePdfs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import PyPDF2, os
+
+    # すべてのPDFファイル名を取得する
+    pdf_files = []
+    for filename in os.listdir('.'):
+        if filename.endswith('.pdf'):
+            pdf_files.append(filename)
+            pdf_files.sort(key=str.lower)
+
+
+if __name__ == '__main__':
+    main()
+
