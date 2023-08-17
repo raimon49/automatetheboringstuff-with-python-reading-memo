f94f68c9359703e3bfe06856e42d0bfc49ad888c
raimon <raimon49@hotmail.com>
2023-08-17 20:25:59 +0900

    Add an annotate comment about Ch 13.3.1

--- ch13/13.3.1_readFromWord.py
+++ ch13/13.3.1_readFromWord.py
@@ -6,10 +6,10 @@ def main():
     import docx
 
     doc = docx.Document('demo.docx')
-    print(len(doc.paragraphs))
+    print(len(doc.paragraphs)) # 7
 
-    print(doc.paragraphs[0].text)
-    print(doc.paragraphs[1].text)
+    print(doc.paragraphs[0].text) # Document Title
+    print(doc.paragraphs[1].text) # A plain paragraph with some bold and some italic
 
 if __name__ == '__main__':
     main()
