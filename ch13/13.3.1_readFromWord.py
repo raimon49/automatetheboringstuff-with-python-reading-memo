3827f34b617e0d1cb8fae5475dd6c50a6bfbc3a8
raimon <raimon49@hotmail.com>
2023-08-18 00:05:33 +0900

    Add an annotate comment about Ch 13.3.1

--- ch13/13.3.1_readFromWord.py
+++ ch13/13.3.1_readFromWord.py
@@ -13,10 +13,10 @@ def main():
 
     print(len(doc.paragraphs[1].runs)) # 4
 
-    print(doc.paragraphs[1].runs[0].text)
-    print(doc.paragraphs[1].runs[1].text)
-    print(doc.paragraphs[1].runs[2].text)
-    print(doc.paragraphs[1].runs[3].text)
+    print(doc.paragraphs[1].runs[0].text) # A plain paragraph with some
+    print(doc.paragraphs[1].runs[1].text) # bold
+    print(doc.paragraphs[1].runs[2].text) #  and some
+    print(doc.paragraphs[1].runs[3].text) # italic
 
 if __name__ == '__main__':
     main()
