e6e3483fc34352b59016c8fc9b0d140879178637
raimon <raimon49@hotmail.com>
2023-08-20 20:45:22 +0900

    Add a code about Ch 13.3.5

---
+++ ch13/13.3.5_editStyleWord.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import docx
+
+    doc = docx.Document('demo.docx')
+    print(doc.paragraphs[0].text)  # Document Title
+    print(doc.paragraphs[1].style) # _ParagraphStyle('Normal') id: 139968275939920
+
+
+if __name__ == '__main__':
+    main()
+
