ac77f0f4961e8ab6db838bd580f30e8a03f8eb29
raimon <raimon49@hotmail.com>
2023-08-22 00:04:48 +0900

    Add an annotate comment about Ch 13.3.5

--- ch13/13.3.5_editStyleWord.py
+++ ch13/13.3.5_editStyleWord.py
@@ -12,6 +12,14 @@ def main():
     doc.paragraphs[0].style = 'Normal'
     print(doc.paragraphs[1].text) # A plain paragraph with some bold and some italic
 
+    print(
+        (doc.paragraphs[1].runs[0].text,
+         doc.paragraphs[1].runs[1].text,
+         doc.paragraphs[1].runs[2].text,
+         doc.paragraphs[1].runs[3].text
+         )
+    ) # ('A plain paragraph with some ', 'bold', ' and some ', 'italic')
+
 
 if __name__ == '__main__':
     main()
