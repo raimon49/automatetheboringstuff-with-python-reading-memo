4c85cfbf15e099dfa6a3ac5ca39e3dab8c189206
raimon <raimon49@hotmail.com>
2023-08-25 20:31:44 +0900

    Add a code about Ch 13.3.6

---
+++ ch13/13.3.6_writeWord.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import docx
+
+    doc = docx.Document()
+    doc.add_paragraph('Hello world!!')
+
+if __name__ == '__main__':
+    main()
+
