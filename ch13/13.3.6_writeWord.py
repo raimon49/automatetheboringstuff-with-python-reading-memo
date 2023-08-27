8aa10853e6e8d2069ad75aba5be401bbd79ec09a
raimon <raimon49@hotmail.com>
2023-08-27 23:26:31 +0900

    Add a code about Ch 13.3.6

--- ch13/13.3.6_writeWord.py
+++ ch13/13.3.6_writeWord.py
@@ -12,6 +12,7 @@ def main():
 
     para_obj1 = doc.add_paragraph('これは第2段落です')
     para_obj2 = doc.add_paragraph('これは第3段落です')
+    para_obj1.add_run(' これは第2段落に追加したテキストです。')
 
 if __name__ == '__main__':
     main()
