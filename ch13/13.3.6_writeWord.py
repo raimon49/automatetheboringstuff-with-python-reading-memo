1af84a376784b02f62b12295e301718cd8c8c575
raimon <raimon49@hotmail.com>
2023-08-28 00:04:25 +0900

    Add a code about Ch 13.3.6

--- ch13/13.3.6_writeWord.py
+++ ch13/13.3.6_writeWord.py
@@ -6,7 +6,7 @@ def main():
     import docx
 
     doc = docx.Document()
-    doc.add_paragraph('Hello world!!')
+    doc.add_paragraph('Hello world!!', 'Title')
 
     # doc.save('helloworld.docx')
 
