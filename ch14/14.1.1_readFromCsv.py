fcee96c9ec66da2927f86a4d39302c8b2858bc75
raimon <raimon49@hotmail.com>
2023-09-08 17:36:02 +0900

    Add a code about Ch 14.1.1

--- ch14/14.1.1_readFromCsv.py
+++ ch14/14.1.1_readFromCsv.py
@@ -26,6 +26,9 @@ def main():
     output_file = open('output.csv', 'w', newline='')
     output_writer = csv.writer(output_file)
     output_writer.writerow(['spam', 'eggs', 'bacon', 'ham'])
+    output_writer.writerow(['Hello, world!', 'eggs', 'bacon', 'ham'])
+    output_writer.writerow([1, 2, 3.141592, 4])
+    output_file.close()
 
 if __name__ == '__main__':
     main()
