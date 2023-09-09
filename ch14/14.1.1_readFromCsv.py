19ea8f370aefec71e382fb3c2a0208b5d87a94b6
raimon <raimon49@hotmail.com>
2023-09-09 17:37:05 +0900

    Refactor code about Ch 14.1.1

--- ch14/14.1.1_readFromCsv.py
+++ ch14/14.1.1_readFromCsv.py
@@ -23,12 +23,11 @@ def main():
         print('Row #' + str(i) + ' ' + str(row))
         i = i + 1
 
-    output_file = open('output.csv', 'w', newline='')
-    output_writer = csv.writer(output_file)
-    output_writer.writerow(['spam', 'eggs', 'bacon', 'ham'])
-    output_writer.writerow(['Hello, world!', 'eggs', 'bacon', 'ham'])
-    output_writer.writerow([1, 2, 3.141592, 4])
-    output_file.close()
+    with open('output.csv', 'w', newline='') as output_file:
+        output_writer = csv.writer(output_file)
+        output_writer.writerow(['spam', 'eggs', 'bacon', 'ham'])
+        output_writer.writerow(['Hello, world!', 'eggs', 'bacon', 'ham'])
+        output_writer.writerow([1, 2, 3.141592, 4])
 
 if __name__ == '__main__':
     main()
