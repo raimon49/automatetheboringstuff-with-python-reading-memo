467c7a381d352e6f2f322f7510b6d79f1cb51225
raimon <raimon49@hotmail.com>
2023-09-14 00:06:48 +0900

    Remove TODO comment in Ch 14.2.1

--- ch14/14.2.1_removeCsvHeader.py
+++ ch14/14.2.1_removeCsvHeader.py
@@ -15,7 +15,7 @@ def main():
 
         print('見出し削除中' + csv_filename + '...')
 
-        # TODO: CSVファイルを読み込む（最初の行をスキップする）
+        # CSVファイルを読み込む（最初の行をスキップする）
         csv_rows = []
         with open(csv_filename) as csv_file_obj:
             reader_obj = csv.reader(csv_file_obj)
