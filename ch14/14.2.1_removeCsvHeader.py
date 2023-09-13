b44e837688ead4614fde75344e8dc2cd94a0cc32
raimon <raimon49@hotmail.com>
2023-09-14 00:05:53 +0900

    Add a code about Ch 14.2.1

--- ch14/14.2.1_removeCsvHeader.py
+++ ch14/14.2.1_removeCsvHeader.py
@@ -16,6 +16,14 @@ def main():
         print('見出し削除中' + csv_filename + '...')
 
         # TODO: CSVファイルを読み込む（最初の行をスキップする）
+        csv_rows = []
+        with open(csv_filename) as csv_file_obj:
+            reader_obj = csv.reader(csv_file_obj)
+            for row in reader_obj:
+                if reader_obj.line_num == 1:
+                    continue
+
+            csv_rows.append(row)
 
         # TODO: CSVファイルを書き出す
 
