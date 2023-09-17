9cd65c03c169b5971b2b3bd720db4d9edaf3f270
raimon <raimon49@hotmail.com>
2023-09-17 20:23:17 +0900

    Add a code about Ch 14.4.1

--- ch14/14.4.1_readWriteJson.py
+++ ch14/14.4.1_readWriteJson.py
@@ -7,9 +7,11 @@ def main():
 
     string_of_json_data = '{"name": "Zophie", "isCat": true, "miceCaught": 0, "felineIQ": null}'
     json_data_as_python_value = json.loads(string_of_json_data)
+    print(type(json_data_as_python_value))
     print(json_data_as_python_value)
 
     string_of_json_data = json.dumps(json_data_as_python_value)
+    print(type(string_of_json_data))
     print(string_of_json_data)
 
 
