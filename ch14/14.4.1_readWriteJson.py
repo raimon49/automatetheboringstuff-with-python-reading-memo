4eb12a943cfa739f4d3616281080d441cb359c0f
raimon <raimon49@hotmail.com>
2023-09-17 20:21:37 +0900

    Add a code about Ch 14.4.1

---
+++ ch14/14.4.1_readWriteJson.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import json
+
+    string_of_json_data = '{"name": "Zophie", "isCat": true, "miceCaught": 0, "felineIQ": null}'
+    json_data_as_python_value = json.loads(string_of_json_data)
+    print(json_data_as_python_value)
+
+
+if __name__ == '__main__':
+    main()
