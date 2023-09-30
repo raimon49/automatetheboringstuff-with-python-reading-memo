e640e2a901d144c5618a82ef12fc1e398cca0fdf
raimon <raimon49@hotmail.com>
2023-09-30 18:32:04 +0900

    Add a code about Ch 15.1.1

--- ch15/15.1.1_calcProd.py
+++ ch15/15.1.1_calcProd.py
@@ -17,6 +17,7 @@ def main():
     prod = calc_prod()
     end_time = time.time()
     print('計算結果は{}桁です。'.format(len(str(prod))))
+    print('計算時間は{}秒でした。'.format(end_time - start_time))
 
 
 if __name__ == '__main__':
