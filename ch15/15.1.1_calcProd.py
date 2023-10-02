d5733a090899d84deeff4209b336cd5442f2de87
raimon <raimon49@hotmail.com>
2023-10-02 19:17:14 +0900

    Add a code about Ch 15.1.1

--- ch15/15.1.1_calcProd.py
+++ ch15/15.1.1_calcProd.py
@@ -3,7 +3,7 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
-    import time, sys
+    import cProfile, time, sys
 
     def calc_prod():
         product = 1
@@ -18,6 +18,9 @@ def main():
     end_time = time.time()
     print('計算結果は{}桁です。'.format(len(str(prod))))
     print('計算時間は{}秒でした。'.format(end_time - start_time))
+
+    # Pythonプロファイラを使った計測
+    cProfile.runctx('calc_prod()', globals(), locals())
 
 
 if __name__ == '__main__':
