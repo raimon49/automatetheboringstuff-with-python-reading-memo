c9a0e372df10d53b2a626036e577bda4d0d3ddd6
raimon <raimon49@hotmail.com>
2023-10-04 19:49:43 +0900

    Add a code about Ch 15.3

---
+++ ch15/15.3_stopwatch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import time
+
+    print('Enterを押すと開始します。その後、Enterを押せば経過時間を表示します。Ctrl + Cで終了します。')
+
+
+if __name__ == '__main__':
+    main()
