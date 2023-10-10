05339455dd8647d773ef67dab77fe60e44b0aa2f
raimon <raimon49@hotmail.com>
2023-10-10 18:41:58 +0900

    Add a code about Ch 15.4

---
+++ ch15/15.4_datetime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import datetime
+
+    print(datetime.datetime.now())
+
+
+if __name__ == '__main__':
+    main()
