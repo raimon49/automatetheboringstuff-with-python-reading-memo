3fc9b08ea103f8b851b4386e9874d727fca837d8
raimon <raimon49@hotmail.com>
2023-10-10 18:43:04 +0900

    Add a code about Ch 15.4

--- ch15/15.4_datetime.py
+++ ch15/15.4_datetime.py
@@ -7,6 +7,8 @@ def main():
 
     print(datetime.datetime.now())
 
+    dt = datetime.datetime(2015, 10, 21, 16, 29, 0)
+    print(dt.year, dt.month, dt.day)
 
 if __name__ == '__main__':
     main()
