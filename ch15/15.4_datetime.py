03867e13b58cb1eb2359347e8f77a497e21c5ac6
raimon <raimon49@hotmail.com>
2023-10-23 20:27:37 +0900

    Add a code about Ch 15.4

--- ch15/15.4_datetime.py
+++ ch15/15.4_datetime.py
@@ -42,7 +42,12 @@ def main():
     print(oct21st.strftime('%I:%M: %p'))         # 04:29: PM
     print(oct21st.strftime("%B of '%y"))         # October of '15
 
-    print(datetime.datetime.strptime('October 21, 2015', '%B %d, %Y'))
+    print(datetime.datetime.strptime(
+        'October 21, 2015', '%B %d, %Y'
+    ))
+    print(datetime.datetime.strptime(
+        '2015/10/21 16:29:00', '%Y/%m/%d %H:%M:%S'
+    ))
 
 if __name__ == '__main__':
     main()
