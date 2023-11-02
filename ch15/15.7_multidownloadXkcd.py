b542ba8f425faa33ac1735d7d304ecf72a1d3b18
raimon <raimon49@hotmail.com>
2023-11-02 13:52:14 +0900

    Remove old implementation

--- ch15/15.7_multidownloadXkcd.py
+++ ch15/15.7_multidownloadXkcd.py
@@ -44,39 +44,6 @@ def main():
     for download_thread in download_threads:
         download_thread.join()
 
-    # 全ての画像URLを見つけるまで繰り返す
-    IS_DOWNLOAD = False
-    while not url.endswith('#'):
-        # ページをダウンロードする
-        res = requests.get(url)
-        res.raise_for_status()
-        soup = bs4.BeautifulSoup(res.text, 'html.parser')
-
-        # コミックの画像URLを見つける
-        comic_elem = soup.select('#comic img')
-        if comic_elem == []:
-            print('コミック画像が見つかりませんでした')
-        else:
-            print('コミック画像が見つかりました')
-            comic_url = 'https:' + comic_elem[0].get('src')
-
-            # 画像をダウンロードする
-            print('画像をダウンロード中 {}...'.format(comic_url))
-            img = requests.get(url)
-            img.raise_for_status()
-
-            if IS_DOWNLOAD:
-                # 画像を./xkcd ディレクトリに保存する
-                with open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb') as img_file:
-                    for chunk in img.iter_content(100000):
-                        img_file.write(chunk)
-            else:
-                pass
-
-            # PrevボタンのURLを取得する
-            prev_link = soup.select('a[rel="prev"]')[0]
-            url = 'https://xkcd.com' + prev_link.get('href')
-
 
 if __name__ == '__main__':
     main()
