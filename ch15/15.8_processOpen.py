4fe4366b76fe97a8eda8ddf90a74f9e5da6829aa
raimon <raimon49@hotmail.com>
2023-11-05 20:03:59 +0900

    Add a code about Ch 15.8

---
+++ ch15/15.8_processOpen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import subprocess
+
+    proc = subprocess.Popen('/usr/bin/ls')
+    print(proc)
+
+
+if __name__ == '__main__':
+    main()
