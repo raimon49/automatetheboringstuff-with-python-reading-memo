7a63782e2eaafe834fe0a226e813cf09114ecf9d
raimon <raimon49@hotmail.com>
2023-11-08 21:17:38 +0900

    Add a code about Ch 15.8

--- ch15/15.8_processOpen.py
+++ ch15/15.8_processOpen.py
@@ -3,7 +3,7 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
-    import subprocess
+    import subprocess, sys
 
     proc = subprocess.Popen('/usr/bin/ls') # <Popen: returncode: None args: '/usr/bin/ls'>
     print(proc.poll() == None) # True
@@ -11,6 +11,8 @@ def main():
     print(proc.poll())         # 0
 
     subprocess.Popen(['/usr/bin/ls', '/usr/bin']) # Popenで呼び出す外部コマンドに引数を渡す（ls /usr/bin）
+
+    subprocess.Popen([sys.executable, '../ch01/1.5_first_program.py'])
 
 
 if __name__ == '__main__':
