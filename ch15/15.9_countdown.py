7d43df3b0655381dd126938c3e4b2f7b0da999ee
raimon <raimon49@hotmail.com>
2023-11-09 20:08:57 +0900

    Add a code about Ch 15.9

---
+++ ch15/15.9_countdown.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import subprocess, time
+
+    time_left = 60
+    while time_left > 0:
+        print(time_left, end='')
+        time.sleep(1)
+        time_left = time_left - 1
+
+
+if __name__ == '__main__':
+    main()
