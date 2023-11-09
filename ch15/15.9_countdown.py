b3d1f036e9d99ac6b243583c03ab909270ca545c
raimon <raimon49@hotmail.com>
2023-11-10 00:32:46 +0900

    Add a code about Ch 15.9

--- ch15/15.9_countdown.py
+++ ch15/15.9_countdown.py
@@ -7,7 +7,7 @@ def main():
 
     time_left = 60
     while time_left > 0:
-        print(time_left, end='')
+        subprocess.Popen(['echo', str(time_left)])
         time.sleep(1)
         time_left = time_left - 1
 
