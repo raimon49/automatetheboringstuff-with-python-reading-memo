a26d0f4a69d5080c1eb07710cf3d93454a3fa983
raimon <raimon49@hotmail.com>
2023-11-13 18:22:36 +0900

    Add a code about Ch 16.2.1

---
+++ ch16/16.2.1_gmail.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import smtplib
+
+    smtp_obj = smtplib.SMTP('smtp.gmail.com', 587)
+    print(type(smtp_obj))
+
+
+if __name__ == '__main__':
+    main()
