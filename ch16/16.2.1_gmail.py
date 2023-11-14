e1b7c8cb32d5108c405d6141fe97c6656a273d9a
raimon <raimon49@hotmail.com>
2023-11-14 19:22:18 +0900

    Add an annotate comment about Ch 16.2.1

--- ch16/16.2.1_gmail.py
+++ ch16/16.2.1_gmail.py
@@ -13,8 +13,8 @@ def main():
     # smtp_ssl = smtplib.SMTP_SSL('smtp.gmai.com', 465)
     # print(type(smtp_ssl))
 
-    print(smtp_obj.ehlo()) # (250, b'smtp.gmail.com at your service, [114.152.124.80]\nSIZE 35882577\n8BITMIME\nSTARTTLS\nENHANCEDSTATUSCODES\nPIPELINING\nCHUNKING\nSMTPUTF8')
-    print(smtp_obj.starttls())
+    print(smtp_obj.ehlo())     # (250, b'smtp.gmail.com at your service, [114.152.124.80]\nSIZE 35882577\n8BITMIME\nSTARTTLS\nENHANCEDSTATUSCODES\nPIPELINING\nCHUNKING\nSMTPUTF8')
+    print(smtp_obj.starttls()) # (220, b'2.0.0 Ready to start TLS')
 
 
 if __name__ == '__main__':
