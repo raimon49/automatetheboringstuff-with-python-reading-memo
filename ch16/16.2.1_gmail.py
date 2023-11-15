52d4fb4b7f63ed3e82e4efc3f1cb0081b953c31b
raimon <raimon49@hotmail.com>
2023-11-15 21:14:15 +0900

    Commented out of using login() and sendmail() code in Ch 16.2.1

--- ch16/16.2.1_gmail.py
+++ ch16/16.2.1_gmail.py
@@ -16,10 +16,10 @@ def main():
     print(smtp_obj.ehlo())     # (250, b'smtp.gmail.com at your service, [114.152.124.80]\nSIZE 35882577\n8BITMIME\nSTARTTLS\nENHANCEDSTATUSCODES\nPIPELINING\nCHUNKING\nSMTPUTF8')
     print(smtp_obj.starttls()) # (220, b'2.0.0 Ready to start TLS')
 
-    print(smtp_obj.login('my_email_address@gmail.com', 'MY_PASS')) # (235, b'2.7.0 Accepted')
-    print(smtp_obj.sendmail('my_email_address@gmail.com',
-                            'recipient@example.com',
-                            'Subject: Solong.\nDearAlice, so long and thanks for all the fish. Sincerely, Bob'))
+    # print(smtp_obj.login('my_email_address@gmail.com', 'MY_PASS')) # (235, b'2.7.0 Accepted')
+    # print(smtp_obj.sendmail('my_email_address@gmail.com',
+    #                         'recipient@example.com',
+    #                         'Subject: Solong.\nDearAlice, so long and thanks for all the fish. Sincerely, Bob'))
 
 
 if __name__ == '__main__':
