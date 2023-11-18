03e72497758f16f10866bf55aae7a949425a7c0e
raimon <raimon49@hotmail.com>
2023-11-18 18:15:53 +0900

    Add a code about Ch 16.4

---
+++ ch16/16.4_imap.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import imapclient, ssl
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+
+
+if __name__ == '__main__':
+    main()
