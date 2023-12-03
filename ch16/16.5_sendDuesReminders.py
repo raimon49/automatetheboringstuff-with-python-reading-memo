bca52efb980a997ccce1ebf27bccd30086a65c88
raimon <raimon49@hotmail.com>
2023-12-03 20:49:43 +0900

    Add a code about Ch 16.5

--- ch16/16.5_sendDuesReminders.py
+++ ch16/16.5_sendDuesReminders.py
@@ -12,6 +12,9 @@ def main():
     last_month = sheet.cell(row=1, column=last_col).value # 最新月を取得
 
     # TODO: 会員の支払い状況を調べる
+    unpaied_members = {}
+    for r in range(2, sheet.max_row + 1):
+        payment = sheet.cell(row=r, column=last_col).value
     # TODO: メールアカウントにログインする
     # TODO: リマインダーメールを送信する
 
