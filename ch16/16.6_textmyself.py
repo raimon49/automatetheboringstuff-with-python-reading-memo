8120990ea98b8dde5c63852867255fde076dba34
raimon <raimon49@hotmail.com>
2023-12-22 18:15:41 +0900

    Add a code about Ch 16.6

--- ch16/16.6_textmyself.py
+++ ch16/16.6_textmyself.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
-
-def main():
-    from twilio.rest import Client
+from twilio.rest import Client
 
 
+def textmyself(message):
     account_SID = 'ACxxxxxxxxxxx'
     auth_token = 'xxxxxxxxxxxxxx'
 
     my_twilio_number = '+12345678901'
     my_cell_phone = '+819012345678'
+    twilio_cli = Client(account_SID, auth_token)
+    message = twilio_cli.messages.create(
+        body='Come on',         # メッセージ本文
+        from_=my_twilio_number, # 米国キャリアの携帯電話番号
+        to=my_cell_phone        # 宛先となる携帯電話番号
+    )
 
-    def textmyself(message):
-        twilio_cli = Client(account_SID, auth_token)
-        message = twilio_cli.messages.create(
-            body='Come on',         # メッセージ本文
-            from_=my_twilio_number, # 米国キャリアの携帯電話番号
-            to=my_cell_phone        # 宛先となる携帯電話番号
-        )
+def main():
+    pass
 
 
 if __name__ == '__main__':
