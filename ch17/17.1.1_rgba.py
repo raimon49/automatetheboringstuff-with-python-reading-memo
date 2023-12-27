7c4f98313160b191989bd2b407cce47179bd433a
raimon <raimon49@hotmail.com>
2023-12-27 18:40:22 +0900

    Add a code about Ch 17.1.1

--- ch17/17.1.1_rgba.py
+++ ch17/17.1.1_rgba.py
@@ -7,6 +7,7 @@ def main():
 
     print(ImageColor.getcolor('red', 'RGBA')) # (255, 0, 0, 255)
     print(ImageColor.getcolor('RED', 'RGBA')) # (255, 0, 0, 255)
+    print(ImageColor.getcolor('Black', 'RGBA'))
 
 
 if __name__ == '__main__':
