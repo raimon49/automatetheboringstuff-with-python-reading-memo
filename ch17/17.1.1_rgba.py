3f38e768a6a3a2f8432d4ff5aa59749360508aa5
raimon <raimon49@hotmail.com>
2023-12-28 14:48:56 +0900

    Add an annotate comment about Ch 17.1.1

--- ch17/17.1.1_rgba.py
+++ ch17/17.1.1_rgba.py
@@ -9,7 +9,7 @@ def main():
     print(ImageColor.getcolor('RED', 'RGBA')) # (255, 0, 0, 255)
     print(ImageColor.getcolor('Black', 'RGBA')) # (0, 0, 0, 255)
     print(ImageColor.getcolor('chocolate', 'RGBA')) # (210, 105, 30, 255)
-    print(ImageColor.getcolor('CornflowerBlue', 'RGBA'))
+    print(ImageColor.getcolor('CornflowerBlue', 'RGBA')) # (100, 149, 237, 255)
 
 
 if __name__ == '__main__':
