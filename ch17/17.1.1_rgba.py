e87d24f3eee64b97188bb0e873d6adac53b51f5e
raimon <raimon49@hotmail.com>
2023-12-30 18:15:57 +0900

    Add an annotate comment about Ch 17.1.1

--- ch17/17.1.1_rgba.py
+++ ch17/17.1.1_rgba.py
@@ -5,10 +5,10 @@
 def main():
     from PIL import ImageColor
 
-    print(ImageColor.getcolor('red', 'RGBA')) # (255, 0, 0, 255)
-    print(ImageColor.getcolor('RED', 'RGBA')) # (255, 0, 0, 255)
-    print(ImageColor.getcolor('Black', 'RGBA')) # (0, 0, 0, 255)
-    print(ImageColor.getcolor('chocolate', 'RGBA')) # (210, 105, 30, 255)
+    print(ImageColor.getcolor('red', 'RGBA'))            # (255, 0, 0, 255)
+    print(ImageColor.getcolor('RED', 'RGBA'))            # (255, 0, 0, 255)
+    print(ImageColor.getcolor('Black', 'RGBA'))          # (0, 0, 0, 255)
+    print(ImageColor.getcolor('chocolate', 'RGBA'))      # (210, 105, 30, 255)
     print(ImageColor.getcolor('CornflowerBlue', 'RGBA')) # (100, 149, 237, 255)
 
 
