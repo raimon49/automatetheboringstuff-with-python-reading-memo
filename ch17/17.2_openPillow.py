e2b9ce4b90f9ef306b0c6817c7328880f3fa8094
raimon <raimon49@hotmail.com>
2023-12-31 17:15:58 +0900

    Add an annotate comment about Ch 17.2

--- ch17/17.2_openPillow.py
+++ ch17/17.2_openPillow.py
@@ -6,7 +6,7 @@ def main():
     from PIL import Image
 
     cat_im = Image.open('zophie.png')
-    print(cat_im)
+    print(cat_im) # <PIL.PngImagePlugin.PngImageFile image mode=RGB size=816x1088 at 0x7F130DE79E50>
 
 
 if __name__ == '__main__':
