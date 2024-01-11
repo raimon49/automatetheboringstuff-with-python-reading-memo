57225902363512e06dd8fa377524ae1b3555cd1b
raimon <raimon49@hotmail.com>
2024-01-11 17:56:43 +0900

    Add a code about Ch 17.2.3

--- ch17/17.2.3_copyPasteImage.py
+++ ch17/17.2.3_copyPasteImage.py
@@ -12,5 +12,8 @@ def main():
     face_im = cat_im.crop((335, 345, 565, 560))
     print(face_im.size) # (230, 215)
 
+    cat_copy_im.paste(face_im, (0, 0))
+    cat_copy_im.paste(face_im, (400, 500))
+
 if __name__ == '__main__':
     main()
