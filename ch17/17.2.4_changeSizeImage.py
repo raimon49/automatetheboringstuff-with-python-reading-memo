f15fedd0aa11b8c3a0dd4a329197706c9b27c1ec
raimon <raimon49@hotmail.com>
2024-01-19 17:53:35 +0900

    Add a code about Ch 17.2.4

---
+++ ch17/17.2.4_changeSizeImage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    from PIL import Image
+
+    cat_im = Image.open('zophie.png')
+    width, height = cat_im.size
+
+    quartersized_im = cat_im.resize((int(width / 2), int(height /2)))
+
+if __name__ == '__main__':
+    main()
