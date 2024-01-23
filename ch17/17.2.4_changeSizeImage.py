c0ce8644a4534d6ae8c537afc65a77caf2dfcb7f
raimon <raimon49@hotmail.com>
2024-01-23 17:14:26 +0900

    Add an annotate comment about Ch 17.2.4

--- ch17/17.2.4_changeSizeImage.py
+++ ch17/17.2.4_changeSizeImage.py
@@ -8,7 +8,7 @@ def main():
     cat_im = Image.open('zophie.png')
     width, height = cat_im.size
 
-    quartersized_im = cat_im.resize((int(width / 2), int(height /2)))
+    quartersized_im = cat_im.resize((int(width / 2), int(height /2))) # 半分の大きさにリサイズ
     quartersized_im.save('quartersized.png')
     svelte_im = cat_im.resize((width, height + 300))
     svelte_im.save('svelte.png')
