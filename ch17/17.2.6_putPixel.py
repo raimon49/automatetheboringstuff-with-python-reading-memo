2746bb4251aec5242ac76273bef514fd74b38d53
raimon <raimon49@hotmail.com>
2024-02-02 18:48:28 +0900

    Add a code about Ch 17.2.6

---
+++ ch17/17.2.6_putPixel.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    from PIL import Image
+
+    im = Image.new('RGBA', (100, 100))
+    print(im.getpixel((0, 0)))
+
+if __name__ == '__main__':
+    main()
