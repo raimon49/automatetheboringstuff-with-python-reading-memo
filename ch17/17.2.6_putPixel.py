6e5912f264b972e4f7846b63f39ef5597d94c75d
raimon <raimon49@hotmail.com>
2024-02-03 18:39:30 +0900

    Add a code about Ch 17.2.6

--- ch17/17.2.6_putPixel.py
+++ ch17/17.2.6_putPixel.py
@@ -3,7 +3,7 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
-    from PIL import Image
+    from PIL import Image, ImageColor
 
     im = Image.new('RGBA', (100, 100))
     print(im.getpixel((0, 0))) # (0, 0, 0, 0)
@@ -11,6 +11,8 @@ def main():
     for y in range(50):
         for x in range(100):
             im.putpixel((x, y), (210, 210, 210))
+
+    darkgray = ImageColor.getcolor('darkgray', 'RGBA')
 
 if __name__ == '__main__':
     main()
