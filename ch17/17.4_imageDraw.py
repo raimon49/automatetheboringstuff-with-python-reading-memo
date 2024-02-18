86d5a4788b99fc48e4aca0592136a1af3342e06f
raimon <raimon49@hotmail.com>
2024-02-18 21:46:58 +0900

    Add a code about Ch 17.4

---
+++ ch17/17.4_imageDraw.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    from PIL import Image, ImageDraw
+
+    im = Image.new('RGBA', (200, 200), 'white')
+    draw = ImageDraw.Draw(im)
+
+if __name__ == '__main__':
+    main()
