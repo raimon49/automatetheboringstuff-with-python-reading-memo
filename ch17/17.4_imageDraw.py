2e3da9757bf189fe9fe76ab9f2f934a90fa20bcc
raimon <raimon49@hotmail.com>
2024-02-21 19:12:24 +0900

    Add a code about Ch 17.4

--- ch17/17.4_imageDraw.py
+++ ch17/17.4_imageDraw.py
@@ -12,6 +12,8 @@ def main():
     draw.rectangle((20, 30, 60, 60), fill='blue')
     draw.ellipse((120, 30, 160, 60), fill='red')
     draw.polygon(((57, 87), (79, 62), (94, 85), (120, 90), (103, 113)), fill='brown')
+    for i in range(100, 200, 10):
+        draw.line([(i, 0), (200, i - 100)], fill='green')
 
 if __name__ == '__main__':
     main()
