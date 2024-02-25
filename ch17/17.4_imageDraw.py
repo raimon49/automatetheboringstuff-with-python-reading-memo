eca9ca9782f52b77e94fc8c7aa20415b3803bbf3
raimon <raimon49@hotmail.com>
2024-02-25 23:05:27 +0900

    Add a code about Ch 17.4

--- ch17/17.4_imageDraw.py
+++ ch17/17.4_imageDraw.py
@@ -3,7 +3,7 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
-    from PIL import Image, ImageDraw
+    from PIL import Image, ImageDraw, ImageFont
 
     im = Image.new('RGBA', (200, 200), 'white')
     draw = ImageDraw.Draw(im)
