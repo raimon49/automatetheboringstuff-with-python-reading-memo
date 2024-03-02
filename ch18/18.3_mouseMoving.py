d2c269c8a8c419c16b5bfb19a9a770f07e7c37c1
raimon <raimon49@hotmail.com>
2024-03-02 18:44:30 +0900

    Add a code about Ch 18.3

---
+++ ch18/18.3_mouseMoving.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import pyautogui
+
+    # 解像度をタプルで出力
+    print(pyautogui.size())
+
+
+if __name__ == '__main__':
+    main()
