a73d4ca5aafc7cbb33b531c34c891f4bc66b94a0
raimon <raimon49@hotmail.com>
2024-03-03 23:14:24 +0900

    Add a code about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -9,6 +9,9 @@ def main():
     print(pyautogui.size())
     width, height = pyautogui.size()
 
+    for i in range(10):
+        pyautogui.moveTo(100, 100, duration=0.25)
+
 
 if __name__ == '__main__':
     main()
