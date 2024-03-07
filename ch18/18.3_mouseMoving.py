8287db6685dda4d65bb0f977c4c0772fc92db514
raimon <raimon49@hotmail.com>
2024-03-07 20:00:30 +0900

    Add a code about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -20,6 +20,7 @@ def main():
         pyautogui.moveRel(100, 0, duration=0.25)
         pyautogui.moveRel(0, 100, duration=0.25)
         pyautogui.moveRel(-100, 0, duration=0.25)
+        pyautogui.moveRel(0, -100, duration=0.25)
 
 if __name__ == '__main__':
     main()
