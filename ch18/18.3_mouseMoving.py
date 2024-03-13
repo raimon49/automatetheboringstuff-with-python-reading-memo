3e500f7f8cb838359b7a2835539b5cdeaad767a8
raimon <raimon49@hotmail.com>
2024-03-13 20:28:32 +0900

    Add a code about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -29,6 +29,7 @@ def main():
     while True:
         x, y = pyautogui.position()
         position_str = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(position, end='')
 
 if __name__ == '__main__':
     main()
