1a2335445e639a7109b81d096d8eb5ca8afd169b
raimon <raimon49@hotmail.com>
2024-03-20 08:29:42 +0900

    Add an annotate comment about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -40,10 +40,10 @@ def main():
 
     distance = 200
     while distance > 0:
-        pyautogui.dragRel(distance, 0, duration=0.2)
+        pyautogui.dragRel(distance, 0, duration=0.2)  # 右へ移動
         distance = distance - 5
-        pyautogui.dragRel(0, distance, duration=0.2)
-        pyautogui.dragRel(-distance, 0, duration=0.2)
+        pyautogui.dragRel(0, distance, duration=0.2)  # 下へ移動
+        pyautogui.dragRel(-distance, 0, duration=0.2) # 左へ移動
         distance = distance - 5
 
 if __name__ == '__main__':
