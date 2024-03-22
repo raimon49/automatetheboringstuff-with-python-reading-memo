cdf9a4a1a227f4afaa6f8e5406126f036398e464
raimon <raimon49@hotmail.com>
2024-03-22 20:02:59 +0900

    Add a code about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -46,6 +46,7 @@ def main():
         pyautogui.dragRel(0, distance, duration=0.2)  # 下へ移動
         pyautogui.dragRel(-distance, 0, duration=0.2) # 左へ移動
         distance = distance - 5
+        pyautogui.dragRel(0, -distance, duration=0.2)
 
 if __name__ == '__main__':
     main()
