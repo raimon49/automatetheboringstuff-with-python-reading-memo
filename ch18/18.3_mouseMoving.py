1a1cd0401e591971e031887f558448d4bad0a50a
raimon <raimon49@hotmail.com>
2024-03-28 18:58:01 +0900

    Add a code about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -59,7 +59,8 @@ def main():
 
     # スクリーンショットを取得（Imageオブジェクトで返される）
     im = pyautogui.screenshot()
-    print(im.getpixel((50, 200))) # (130, 135, 144)
+    print(im.getpixel((0, 0)))    # (130, 135, 144)
+    print(im.getpixel((50, 200))) # (255, 135, 144)
 
 if __name__ == '__main__':
     main()
