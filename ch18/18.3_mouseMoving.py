336a6aa999abc09bb000c72866316f33ac3b3f3a
raimon <raimon49@hotmail.com>
2024-04-28 08:57:30 +0900

    Add a code about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -3,7 +3,7 @@
 # vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 
 def main():
-    import pyautogui, pyperclip
+    import pyautogui, pyperclip, time
 
     # 解像度をタプルで取得し出力・変数に代入
     print(pyautogui.size())
