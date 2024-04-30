7488ac74f28398b1fc7231c7bb01a7b8c42e72de
raimon <raimon49@hotmail.com>
2024-04-30 17:22:26 +0900

    Add a code about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -101,6 +101,7 @@ def main():
     def comment_after_delay():
         pyautogui.click(100, 100)
         pyautogui.typewrite('In IDLE, Alt-3 comments out a line.')
+        times.sleep(2)
 
 
 if __name__ == '__main__':
