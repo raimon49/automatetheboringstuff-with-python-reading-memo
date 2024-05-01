87857fee6914af431d61c7ed6be3d01d21306d9d
raimon <raimon49@hotmail.com>
2024-05-01 14:06:50 +0900

    Add a code about Ch 18.3

--- ch18/18.3_mouseMoving.py
+++ ch18/18.3_mouseMoving.py
@@ -102,6 +102,7 @@ def main():
         pyautogui.click(100, 100)
         pyautogui.typewrite('In IDLE, Alt-3 comments out a line.')
         times.sleep(2)
+        pyautogui.hotkey('alt', '3')
 
 
 if __name__ == '__main__':
