c2f79e1fb5b322fb8e9bb2d4827d99018c0be590
raimon <raimon49@hotmail.com>
2024-05-04 00:10:02 +0900

    Add a code about Ch 18.10

---
+++ ch18/18.10_formFillter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+
+def main():
+    import pyautogui, time
+
+    name_field = (648, 319)
+    submit_button = (651, 817)
+    submit_button_color = (75, 141, 249)
+    submit_anoter_link = (760, 224)
+
+
+if __name__ == '__main__':
+    main()
